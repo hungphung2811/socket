5a4e3c26c1a1674fbaae97ddffc3e18b2f9a601e
hungphung2811 <70595130+hungphung2811@users.noreply.github.com>
2022-01-09 19:51:44 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,22 +22,22 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+    console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
+server.listen(process.env.PORT || 3001, () => {
+    console.log("SERVER RUNNING");
 });
